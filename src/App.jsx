3ca8974caa68d104023bbfc842d151dc1d8781c3
unknown <taiye.akinlade1@gmail.com>
2023-08-25 00:37:08 +0100

    update on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SingUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+import Offers from "./pages/Offers"
+import Header from "./component/Header"
 
 function App() {
 
   return (
     <>
-    <h1 className='text-blue-900'>Hello</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SingUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </> 
   )
 }
