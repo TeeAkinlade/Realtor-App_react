28f74acdcd91328e01bd5b9937abaeb40e328905
TeeAkinlade <taiye.akinlade1@gmail.com>
2023-09-11 13:39:46 +0100

    copy link

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -64,7 +64,7 @@ const Listing = () => {
                     </SwiperSlide>
                 ))}
             </Swiper>
-            <div className="fixed top-[13%] right-[5%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={() => {
+            <div className="fixed top-[15%] sm:top-[13%] right-[5%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center" onClick={() => {
                 //to copy the window location href
                 navigator.clipboard.writeText(window.location.href)
                 setShareLinkCopied(true)
@@ -75,7 +75,7 @@ const Listing = () => {
                 <FaShare className='text-lg text-slate-500' />
             </div>
             {shareLinkCopied && (
-                <p className="fixed top-[23%] right-[5%] font-semibold boder-2 border-gray-400 rounded-md bg-white z-10 p-2 ">Linked copied</p>
+                <p className="fixed top-[30%] sm:top-[25%] right-[5%] font-semibold boder-2 border-gray-400 rounded-md bg-white z-10 p-2 ">Linked copied</p>
             )}
 
             <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto m-4 p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
